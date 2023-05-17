0569d3b70cd7274032ca5612a9e4c99ca8cae61b
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-05-17 03:19:41 +0000

    .

--- code-challenges/challenges-18.js
+++ code-challenges/challenges-18.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 01:
@@ -15,23 +15,22 @@
 //
 
 const wordLength = (str) => {
-    // write your code here
-    let array=str.split(" ")
-    console.log(array);
-    let indexOfWord =Math.round((array.length-1)/2)
-    console.log(indexOfWord);
-    for (let i=0; i<array.length;i++){
-        let word =array[indexOfWord]
-        console.log(word)
-        let lengthOfWord=word.length
-        return lengthOfWord
-    }
-}
+  // write your code here
+  let array = str.split(" ");
+  console.log(array);
+  let indexOfWord = Math.round((array.length - 1) / 2);
+  console.log(indexOfWord);
+  for (let i = 0; i < array.length; i++) {
+    let word = array[indexOfWord];
+    console.log(word);
+    let lengthOfWord = word.length;
+    return lengthOfWord;
+  }
+};
 // console.log(wordLength("You can't handle the truth!"))
 // console.log(wordLength("You're gonna need a bigger boat"))
 
 // -------------------------------------------------------------------------------------------------------
-
 
 // -------------------------------------------------------------------------------------------------------
 // Challenge 02:
@@ -53,27 +52,23 @@ const wordLength = (str) => {
 // Output: false
 
 const wordLetters = (str1, str2) => {
-    // write your code here
-    for(let i=0;i<str1.length ;i++){
-        if(str1.length===str2.length){
-            // console.log("hi")
-        //   console.log(str2.includes(str1[i]))
-          if(str2.includes(str1[i])===true){
-            return true
-          }
-          else {
-            return false
-          }
-
-        }
-        else{
-
-            return false
-        }
+  // write your code here
+  for (let i = 0; i < str1.length; i++) {
+    if (str1.length === str2.length) {
+      // console.log("hi")
+      //   console.log(str2.includes(str1[i]))
+      if (str2.includes(str1[i]) === true) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
     }
-}
-console.log(wordLetters("dad","add"))
-console.log(wordLetters("table","label"))
+  }
+};
+// console.log(wordLetters("dad","add"))
+// console.log(wordLetters("table","label"))
 
 // -------------------------------------------------------------------------------------------------------
 
@@ -96,9 +91,41 @@ console.log(wordLetters("table","label"))
 // Output: 4
 //
 
+
+
+// function appendAndDelete(s, t, k) {
+//     // check if k is too long
+//   if (k >= s.length + t.length) {
+//     return "Yes";
+//   }
+
+//    // calculate the common characrers
+//   let commonLength = 0;
+//   for (let i = 0; i < Math.min(s.length, t.length); i++) {
+//     if (s[i] !== t[i]) {
+//       break;
+//     }
+//     commonLength++;
+//   }
+//   // calculate the requierd operatins
+//   const operationsRequired = s.length + t.length - 2 * commonLength;
+// //   check if k === or bigger than the requierd operations 
+//   if (operationsRequired % 2 === k % 2 && operationsRequired <= k) {
+//     return "Yes";
+//   }
+//   else {
+//     return "No";
+//   }
+// }
+// console.log(appendAndDelete("hackerhappy", "hackerrank", 9));
+// console.log(appendAndDelete("ashley", "ash",2));
+
+
+
+
 const targetIndex = (arr, int) => {
-    // write your code here
-}
+  // write your code here
+};
 // -------------------------------------------------------------------------------------------------------
 
 module.exports = { wordLength, wordLetters, targetIndex };
